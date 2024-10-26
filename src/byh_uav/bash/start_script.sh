73bc873a6e298c026a5c598150738a439766eb05
WhisterKuper <2020302121154@whu.edu.cn>
2024-10-26 13:56:40 +0800

    增加脚本

--- src/byh_uav/bash/start_script.sh
+++ src/byh_uav/bash/start_script.sh
@@ -4,44 +4,39 @@ source /opt/ros/noetic/setup.zsh
 
 sleep 1;
 sudo ifconfig eth0 192.168.1.50
-
-sleep 1;
-gnome-terminal -- zsh -c "sudo ptp4l -m -S -l 6 -i eth0; exec zsh"
-echo “ptp sync successfully started”
-
-sleep 1;
-source ~/Ros/livox_ws/devel/setup.zsh 
-gnome-terminal -- zsh -c "ulimit -c unlimited && echo core-%e-%p-%t | sudo tee /proc/sys/kernel/core_pattern && sudo ifconfig eth0 192.168.1.50 && roslaunch livox_ros_driver2 rviz_MID360.launch; exec zsh"
-echo “MID360  successfully started”
-
-# sleep 1;
-# source ~/Ros/livox_ws/devel/setup.zsh 
-# gnome-terminal -- zsh -c "ulimit -c unlimited && echo core-%e-%p-%t | sudo tee /proc/sys/kernel/core_pattern && sudo ifconfig eth0 192.168.1.50 && roslaunch livox_ros_driver livox_lidar_rviz.launch; exec zsh"
-# echo “MID70  successfully started”
-
-# sleep 1;
-# source ~/Ros/ars548_ws/devel/setup.zsh 
-# gnome-terminal -- zsh -c "sudo -u root zsh -c \" ulimit -c unlimited && echo core-%e-%p-%t | sudo tee /proc/sys/kernel/core_pattern && sudo ifconfig eth0 192.168.1.50 && source /home/kuper/Ros/ars548_ws/devel/setup.zsh && roslaunch ars540_msgs ars540.launch  \"; exec zsh"
-# echo “ARS548  successfully started”
-
-# sleep 1;
-# source ~/Ros/spinnaker_ws/devel/setup.zsh 
-# gnome-terminal -- zsh -c "roslaunch spinnaker_sdk_camera_driver acquisition.launch; exec zsh"
-# echo "FLIR  successfully started”
-
-sleep 1;
+sudo ifconfig eth1 192.168.0.144
 sudo chmod 777 /dev/ttyTHS0
-sleep 1;
-sudo chmod 777 /dev/ttyACM0
 
 sleep 1;
-source ~/Ros/byh_uav_ws/devel/setup.zsh 
-gnome-terminal -- zsh -c "ulimit -c unlimited && echo core-%e-%p-%t | sudo tee /proc/sys/kernel/core_pattern && roslaunch byh_uav base_serial.launch; exec zsh"
-echo “base_serial  successfully started”
 
-sleep 1;
-gnome-terminal -- zsh -c " sudo -u root zsh -c \" ulimit -c unlimited && echo core-%e-%p-%t | sudo tee /proc/sys/kernel/core_pattern && source /home/kuper/Ros/byh_uav_ws/devel/setup.zsh  && rosrun byh_uav_pps byh_uav_pps /dev/pps1 \"; exec zsh"
-echo "pps  successfully started"
+# 开启ptp主时钟同步
+gnome-terminal --tab --title="ptp同步" -- zsh -c "sudo ptp4l -m -S -l 6 -i eth0; exec zsh" && 
+# BYHUAV 开启
+gnome-terminal --tab --title="BYHUAV" -- zsh -c "ulimit -c unlimited && echo core-%e-%p-%t | sudo tee /proc/sys/kernel/core_pattern && source ~/Ros/byh_uav_ws/devel/setup.zsh && roslaunch byh_uav base_serial.launch; exec zsh" && 
+sleep 1 &&
+# PPS同步 开启
+gnome-terminal --tab --title="PPS同步" -- zsh -c "sudo -u root zsh -c \" ulimit -c unlimited && echo core-%e-%p-%t | sudo tee /proc/sys/kernel/core_pattern && source /home/kuper/Ros/byh_uav_ws/devel/setup.zsh  && rosrun byh_uav_pps byh_uav_pps /dev/pps1 \"; exec zsh" && 
+# MID360 开启
+# gnome-terminal --tab --title="MID360" -- zsh -c "ulimit -c unlimited && echo core-%e-%p-%t | sudo tee /proc/sys/kernel/core_pattern && sudo ifconfig eth0 192.168.1.50 && roslaunch livox_ros_driver2 rviz_MID360.launch; exec zsh" && 
+# MID70 开启
+# gnome-terminal --tab --title="MID70" -- zsh -c "ulimit -c unlimited && echo core-%e-%p-%t | sudo tee /proc/sys/kernel/core_pattern && sudo ifconfig eth0 192.168.1.50 && roslaunch livox_ros_driver livox_lidar_rviz.launch; exec zsh" && 
+# ARS548 开启
+# gnome-terminal --tab --title="ARS548" -- zsh -c "sudo -u root zsh -c \" ulimit -c unlimited && echo core-%e-%p-%t | sudo tee /proc/sys/kernel/core_pattern && sudo ifconfig eth0 192.168.1.50 && source /home/kuper/Ros/ars548_ws/devel/setup.zsh && roslaunch ars540_msgs ars540.launch  \"; exec zsh" && 
+# FLIR 开启
+# gnome-terminal --tab --title="FLIR" -- zsh -c "roslaunch spinnaker_sdk_camera_driver acquisition.launch; exec zsh" && 
+# 输出信息
+gnome-terminal --tab --title="ADIS16470" -- zsh -c "source ~/Ros/byh_uav_ws/devel/setup.zsh && rostopic echo /byh_uav/ADIS16470 ; exec zsh" && 
+gnome-terminal --tab --title="D435i" -- zsh -c "source ~/Ros/byh_uav_ws/devel/setup.zsh && rostopic echo /byh_uav/D435i ; exec zsh" && 
+gnome-terminal --tab --title="RM3100" -- zsh -c "source ~/Ros/byh_uav_ws/devel/setup.zsh && rostopic echo /byh_uav/RM3100 ; exec zsh" && 
+gnome-terminal --tab --title="ICM42688" -- zsh -c "source ~/Ros/byh_uav_ws/devel/setup.zsh && rostopic echo /byh_uav/ICM42688 ; exec zsh" &&
+gnome-terminal --tab --title="ZEDF9P" -- zsh -c "source ~/Ros/byh_uav_ws/devel/setup.zsh && rostopic echo /byh_uav/ZEDF9P ; exec zsh"
+
+echo "ptp sync successfully started"
+echo "base_serial  successfully started"
+echo "MID360  successfully started"
+echo "MID70  successfully started"
+echo "ARS548  successfully started"
+echo "FLIR  successfully started"
 
 sleep 1;
 wait;

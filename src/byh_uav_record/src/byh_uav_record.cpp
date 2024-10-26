45714a0bfce34b9da8c7a5d24f25084bc42d3144
WhisterKuper <2020302121154@whu.edu.cn>
2024-10-26 13:58:48 +0800

    删除 count 记录

--- src/byh_uav_record/src/byh_uav_record.cpp
+++ src/byh_uav_record/src/byh_uav_record.cpp
@@ -22,8 +22,7 @@ void ADIS16470_Callback(const byh_uav::uav_imu::ConstPtr& msg)
     // 文件已经打开
     if ( ofs_adis16470.is_open() && (acqusition == true) ) 
     {
-        ofs_adis16470 << msg->count << "\t"
-            << (uint64_t)(msg->gyro_gps_time * 10000000000) / 10000000000 << "."
+        ofs_adis16470 << (uint64_t)(msg->gyro_gps_time * 10000000000) / 10000000000 << "."
             << setw(10) << setfill('0') << (uint64_t)(msg->gyro_gps_time * 10000000000) % 10000000000 << "\t"
             << fixed<<setprecision(10) << msg->angular_velocity.x << "\t"
             << fixed<<setprecision(10) << msg->angular_velocity.y << "\t"
@@ -40,8 +39,7 @@ void ICM42688_Callback(const byh_uav::uav_imu::ConstPtr& msg)
     // 文件已经打开
     if ( ofs_icm42688.is_open() && (acqusition == true) ) 
     {
-        ofs_icm42688 << msg->count << "\t" 
-            << (uint64_t)(msg->gyro_gps_time * 10000000000) / 10000000000 << "."
+        ofs_icm42688 << (uint64_t)(msg->gyro_gps_time * 10000000000) / 10000000000 << "."
             << setw(10) << setfill('0') << (uint64_t)(msg->gyro_gps_time * 10000000000) % 10000000000 << "\t"
             << fixed<<setprecision(10) << msg->angular_velocity.x << "\t"
             << fixed<<setprecision(10) << msg->angular_velocity.y << "\t"
@@ -58,8 +56,7 @@ void BMI088_Callback(const byh_uav::uav_imu::ConstPtr& msg)
     // 文件已经打开
     if ( ofs_bmi088.is_open() && (acqusition == true) ) 
     {
-        ofs_bmi088 << msg->count << "\t"
-            << (uint64_t)(msg->gyro_gps_time * 10000000000) / 10000000000 << "."
+        ofs_bmi088 << (uint64_t)(msg->gyro_gps_time * 10000000000) / 10000000000 << "."
             << setw(10) << setfill('0') << (uint64_t)(msg->gyro_gps_time * 10000000000) % 10000000000 << "\t"
             << fixed<<setprecision(10) << msg->angular_velocity.x << "\t"
             << fixed<<setprecision(10) << msg->angular_velocity.y << "\t"
@@ -78,8 +75,7 @@ void RM3100_Callback(const byh_uav::uav_magnet::ConstPtr& msg)
     // 文件已经打开
     if ( ofs_rm3100.is_open() && (acqusition == true) ) 
     {
-        ofs_rm3100 << msg->count << "\t"
-            << (uint64_t)(msg->magnet_gps_time * 10000000000) / 10000000000 << "."
+        ofs_rm3100 << (uint64_t)(msg->magnet_gps_time * 10000000000) / 10000000000 << "."
             << setw(10) << setfill('0') << (uint64_t)(msg->magnet_gps_time * 10000000000) % 10000000000 << "\t"
             << fixed<<setprecision(10) << msg->magnet.x << "\t"
             << fixed<<setprecision(10) << msg->magnet.y << "\t"
@@ -93,8 +89,7 @@ void AK8975_Callback(const byh_uav::uav_magnet::ConstPtr& msg)
     // 文件已经打开
     if ( ofs_ak8975.is_open() && (acqusition == true) ) 
     {
-        ofs_ak8975 << msg->count << "\t"
-            << (uint64_t)(msg->magnet_gps_time * 10000000000) / 10000000000 << "."
+        ofs_ak8975 << (uint64_t)(msg->magnet_gps_time * 10000000000) / 10000000000 << "."
             << setw(10) << setfill('0') << (uint64_t)(msg->magnet_gps_time * 10000000000) % 10000000000 << "\t"
             << fixed<<setprecision(10) << msg->magnet.x << "\t"
             << fixed<<setprecision(10) << msg->magnet.y << "\t"
@@ -108,8 +103,7 @@ void MS5611_Callback(const byh_uav::uav_barometer::ConstPtr& msg)
     // 文件已经打开
     if ( ofs_ms5611.is_open() && (acqusition == true) ) 
     {
-        ofs_ms5611 << msg->count << "\t"
-            << (uint64_t)(msg->data_gps_time * 10000000000) / 10000000000 << "."
+        ofs_ms5611 << (uint64_t)(msg->data_gps_time * 10000000000) / 10000000000 << "."
             << setw(10) << setfill('0') << (uint64_t)(msg->data_gps_time * 10000000000) % 10000000000 << "\t"
             << fixed<<setprecision(10) << msg->height << "\t"<< "\n";
     }
@@ -121,8 +115,7 @@ void ZEDF9P_Callback(const byh_uav::uav_gps::ConstPtr& msg)
     // 文件已经打开
     if ( ofs_zedf9p.is_open() && (acqusition == true) ) 
     {
-        ofs_zedf9p << msg->count << "\t"
-            << (uint64_t)(msg->pps_gps_time * 10000000000) / 10000000000 << "."
+        ofs_zedf9p << (uint64_t)(msg->pps_gps_time * 10000000000) / 10000000000 << "."
             << setw(10) << setfill('0') << (uint64_t)(msg->pps_gps_time * 10000000000) % 10000000000 << "\t"
             << fixed<<setprecision(10) << msg->longitude << "\t"
             << fixed<<setprecision(10) << msg->latitude << "\t"
@@ -139,8 +132,7 @@ void GPSPPS_Callback(const byh_uav::uav_pps::ConstPtr& msg)
     // 文件已经打开
     if ( ofs_gpspps.is_open() && (acqusition == true) ) 
     {
-        ofs_gpspps << msg->count << "\t"
-            << (uint64_t)(msg->pulse_gps_time.sec * 10000000000) / 10000000000 << "."
+        ofs_gpspps << (uint64_t)(msg->pulse_gps_time.sec * 10000000000) / 10000000000 << "."
             << setw(9) << setfill('0') << right << (uint64_t)(msg->pulse_gps_time.nsec) << "\t"
             << (uint64_t)(msg->pulse_sys_time.sec * 10000000000) / 10000000000 << "."
             << setw(9) << setfill('0') << right << (uint64_t)(msg->pulse_sys_time.nsec) << "\t" << "\n";
